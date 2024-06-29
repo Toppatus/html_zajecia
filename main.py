5efba463d17348f25e0b9974a4bc79203de415d3
Tomasz <tomaszkortyka@gmail.com>
2024-06-29 10:54:50 +0200

    k

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+print(__name__)
+
 @app.route("/")
 def hello_world():
     return "<h1>Goodbye, World!</h1>"
+
+@app.route("/podstrona")
+def podstrona():
+    return "<h1>Jakaś podstrona </h1>"
 
 app.run(debug=True)
